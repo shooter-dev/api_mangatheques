c651177f0ac3378a9582d75b57491acbc18c7e86
Vincent PLATEVOET <vincentbleach@gmail.com>
2021-11-15 21:14:44 +0100

    build: config phpunit

---
+++ tests/Unit/FooTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class FooTest extends TestCase
+{
+    public function testFoo(): void
+    {
+        $this->assertTrue(true);
+    }
+}
