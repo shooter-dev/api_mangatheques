a58d01d9192e6dfac8f2c1768c786aee9c0c0e64
Vincent PLATEVOET <vincentbleach@gmail.com>
2021-11-15 22:05:35 +0100

    feat: implement User entity (#13)

---
+++ src/Entity/User.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[Entity]
+#[Table(name: 'user')]
+#[UniqueEntity(fields: 'email')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[Id]
+    #[Column(type: 'integer')]
+    #[GeneratedValue]
+    private ?int $id = null;
+
+    #[Column(unique: true)]
+    private string $email;
+
+    #[Column]
+    private string $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return array<array-key, string>
+     */
+    public function getRoles(): array
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    public function eraseCredentials(): void
+    {
+    }
+
+    public function getUsername(): string
+    {
+        return $this->email;
+    }
+}
