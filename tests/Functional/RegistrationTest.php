b4e919b9673c43d055daf6a9ea30fac8cc43059b
Vincent PLATEVOET <vincentbleach@gmail.com>
2021-11-16 01:21:55 +0100

    test: registration (#19)

---
+++ tests/Functional/RegistrationTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
+
+final class RegistrationTest extends ApiTestCase
+{
+    public function testIfUserIsRegistered(): void
+    {
+        static::createClient()->request(
+            'POST',
+            '/api/users',
+            [
+                'json' => [
+                    'email' => 'user+2@email.com',
+                    'password' => 'password',
+                    'firstName' => 'John',
+                    'lastName' => 'Doe',
+                ],
+            ]
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->assertMatchesResourceItemJsonSchema(User::class);
+    }
+}
