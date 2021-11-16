1a0b7f796b49b17fc6246aea9e4fb6d397052097
Vincent PLATEVOET <vincentbleach@gmail.com>
2021-11-16 03:44:04 +0100

    test: administration show publisher (#28)

--- tests/Functional/Admin/PublisherTest.php
+++ tests/Functional/Admin/PublisherTest.php
@@ -37,4 +37,31 @@ final class PublisherTest extends WebTestCase
 
         $this->assertCount(9, $crawler->filter('article.content table tbody tr'));
     }
+
+    public function testIfPublisherIsShown(): void
+    {
+        $client = static::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        $client->loginUser($entityManager->find(Administrator::class, 1), 'admin');
+
+        $crawler = $client->request(
+            'GET',
+            $adminUrlGenerator
+                ->setController(PublisherCrudController::class)
+                ->setAction(Action::DETAIL)
+                ->setEntityId(1)
+                ->generateUrl()
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        $this->assertStringContainsString('1', $crawler->filter('dl.datalist div:first-child dd')->text());
+        $this->assertStringContainsString('publisher+1', $crawler->filter('dl.datalist div:last-child dd')->text());
+    }
 }
