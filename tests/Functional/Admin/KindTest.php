186a74d250304cba7db5afd682a2ff9aeffff6bd
shooterdev <31919820+shooter-dev@users.noreply.github.com>
2021-11-22 13:32:15 +0100

    test: administration show kind (#44)

--- tests/Functional/Admin/KindTest.php
+++ tests/Functional/Admin/KindTest.php
@@ -37,4 +37,37 @@ final class KindTest extends WebTestCase
 
         $this->assertCount(9, $crawler->filter('article.content table tbody tr'));
     }
+
+    public function testIfKindIsShown(): void
+    {
+        $client = static::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        $client->loginUser($entityManager->find(Administrator::class, 1), 'admin');
+
+        $crawler = $client->request(
+            'GET',
+            $adminUrlGenerator
+                ->setController(KindCrudController::class)
+                ->setAction(Action::DETAIL)
+                ->setEntityId(1)
+                ->generateUrl()
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        $this->assertStringContainsString(
+            '1',
+            $crawler->filter('dl.datalist div:first-child dd')->text()
+        );
+        $this->assertStringContainsString(
+            'kind+1',
+            $crawler->filter('dl.datalist div:nth-child(2) dd')->text()
+        );
+    }
 }
