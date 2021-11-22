3cffa150af97eb8bae818a59d4d8c65546d8cc98
shooterdev <31919820+shooter-dev@users.noreply.github.com>
2021-11-22 14:47:44 +0100

    test: administration update genre (#53)

--- tests/Functional/Admin/GenreTest.php
+++ tests/Functional/Admin/GenreTest.php
@@ -6,6 +6,7 @@ namespace App\Tests\Functional\Admin;
 
 use App\Controller\Admin\GenreCrudController;
 use App\Entity\Administrator;
+use App\Entity\Genre;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
@@ -69,5 +70,39 @@ final class GenreTest extends WebTestCase
             'genre+1',
             $crawler->filter('dl.datalist div:nth-child(2) dd')->text()
         );
+    }
+
+    public function testIfGenreIsUpdated(): void
+    {
+        $client = static::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        $client->loginUser($entityManager->find(Administrator::class, 1), 'admin');
+
+        $client->request(
+            'GET',
+            $adminUrlGenerator
+                ->setController(GenreCrudController::class)
+                ->setAction(Action::EDIT)
+                ->setEntityId(1)
+                ->generateUrl()
+        );
+
+        $client->submitForm('Sauvegarder les modifications', [
+            'Genre[title]' => 'Modifié',
+        ]);
+
+        $this->assertResponseRedirects();
+
+        $client->followRedirect();
+
+        $genre = $entityManager->find(Genre::class, 1);
+
+        $this->assertEquals('Modifié', $genre->getTitle());
     }
 }
