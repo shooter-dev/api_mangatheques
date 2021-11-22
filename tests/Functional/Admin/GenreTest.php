94c96ab3f4d3693da6054f6bc33f88552f77f438
shooterdev <31919820+shooter-dev@users.noreply.github.com>
2021-11-22 14:47:09 +0100

    test: administration show genre (#52)

--- tests/Functional/Admin/GenreTest.php
+++ tests/Functional/Admin/GenreTest.php
@@ -37,4 +37,37 @@ final class GenreTest extends WebTestCase
 
         $this->assertCount(9, $crawler->filter('article.content table tbody tr'));
     }
+
+    public function testIfGenreIsShown(): void
+    {
+        $client = static::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        $client->loginUser($entityManager->find(Administrator::class, 1), 'admin');
+
+        $crawler = $client->request(
+            'GET',
+            $adminUrlGenerator
+                ->setController(GenreCrudController::class)
+                ->setAction(Action::DETAIL)
+                ->setEntityId(1)
+                ->generateUrl()
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        $this->assertStringContainsString(
+            '1',
+            $crawler->filter('dl.datalist div:first-child dd')->text()
+        );
+        $this->assertStringContainsString(
+            'genre+1',
+            $crawler->filter('dl.datalist div:nth-child(2) dd')->text()
+        );
+    }
 }
