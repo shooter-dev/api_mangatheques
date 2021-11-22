ccc072eaad35210e53f0fd4f8ce8b867b3db4c6a
shooterdev <31919820+shooter-dev@users.noreply.github.com>
2021-11-22 12:29:02 +0100

    test: administration update author (#37)

--- tests/Functional/Admin/AuthorTest.php
+++ tests/Functional/Admin/AuthorTest.php
@@ -6,6 +6,7 @@ namespace App\Tests\Functional\Admin;
 
 use App\Controller\Admin\AuthorCrudController;
 use App\Entity\Administrator;
+use App\Entity\Author;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
@@ -72,5 +73,41 @@ final class AuthorTest extends WebTestCase
             'first_name+1',
             $crawler->filter('dl.datalist div:nth-child(3) dd')->text()
         );
+    }
+
+    public function testIfAuthorIsUpdated(): void
+    {
+        $client = static::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        $client->loginUser($entityManager->find(Administrator::class, 1), 'admin');
+
+        $client->request(
+            'GET',
+            $adminUrlGenerator
+                ->setController(AuthorCrudController::class)
+                ->setAction(Action::EDIT)
+                ->setEntityId(1)
+                ->generateUrl()
+        );
+
+        $client->submitForm('Sauvegarder les modifications', [
+            'Author[lastName]' => 'Modifié',
+            'Author[firstName]' => 'Modifié',
+        ]);
+
+        $this->assertResponseRedirects();
+
+        $client->followRedirect();
+
+        $author = $entityManager->find(Author::class, 1);
+
+        $this->assertEquals('Modifié', $author->getLastName());
+        $this->assertEquals('Modifié', $author->getFirstName());
     }
 }
