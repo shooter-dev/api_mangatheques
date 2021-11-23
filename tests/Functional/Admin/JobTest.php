cb61e88057bdcb788924b7bc3e2bf36c2d5b9831
shooterdev <31919820+shooter-dev@users.noreply.github.com>
2021-11-23 05:04:10 +0100

    test: administration list job (#60)

---
+++ tests/Functional/Admin/JobTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Admin;
+
+use App\Controller\Admin\JobCrudController;
+use App\Entity\Administrator;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class JobTest extends WebTestCase
+{
+    public function testIfJobsAreListed(): void
+    {
+        $client = static::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        $client->loginUser($entityManager->find(Administrator::class, 1), 'admin');
+
+        $crawler = $client->request(
+            'GET',
+            $adminUrlGenerator
+                ->setController(JobCrudController::class)
+                ->setAction(Action::INDEX)
+                ->generateUrl()
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        $this->assertCount(9, $crawler->filter('article.content table tbody tr'));
+    }
+}
