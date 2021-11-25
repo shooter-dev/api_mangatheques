2a51bde7bb86f3ae0fc9f63fb9db9056253d8e41
shooterdev <31919820+shooter-dev@users.noreply.github.com>
2021-11-25 09:10:48 +0100

    test: administration show serie (#69)

--- tests/Functional/Admin/SerieTest.php
+++ tests/Functional/Admin/SerieTest.php
@@ -37,4 +37,46 @@ final class SerieTest extends WebTestCase
 
         $this->assertCount(9, $crawler->filter('article.content table tbody tr'));
     }
+
+    public function testIfSerieIsShown(): void
+    {
+        $client = static::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        $client->loginUser($entityManager->find(Administrator::class, 1), 'admin');
+
+        $crawler = $client->request(
+            'GET',
+            $adminUrlGenerator
+                ->setController(SerieCrudController::class)
+                ->setAction(Action::DETAIL)
+                ->setEntityId(1)
+                ->generateUrl()
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        $this->assertStringContainsString(
+            '1',
+            $crawler->filter('dl.datalist div:first-child dd')->text()
+        );
+        $this->assertStringContainsString(
+            'serie+1',
+            $crawler->filter('dl.datalist div:nth-child(2) dd')->text()
+        );
+        'Oui' === $crawler->filter('dl.datalist div:nth-child(3) dd')->text()
+            ? $this->assertStringContainsString(
+            'oui', $crawler->filter(
+            'dl.datalist div:nth-child(3) dd')->text()
+        )
+            : $this->assertStringContainsString(
+            'Non', $crawler->filter(
+            'dl.datalist div:nth-child(3) dd')->text()
+        );
+    }
 }
